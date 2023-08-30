e4539a69a7a335bec4c1224df2ec110a79292568
bri-engineer <brian.kelly.hwang@gmail.com>
2023-08-30 19:00:13 -0400

    branch testing

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+console.log("hello");
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
