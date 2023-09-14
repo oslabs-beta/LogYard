c774e8cd00a913085c336273458a607c47575df2
Geoff Sun <geoffsun2@gmail.com>
2023-09-14 11:38:26 -0400

    worked in dev on styling

--- client/main/utility/InputBar/ButtonInput.jsx
+++ client/main/utility/InputBar/ButtonInput.jsx
@@ -13,7 +13,7 @@ import React from 'react';
 
 const ButtonInput = ({ label, onClick })=>{
   return (
-    <button onClick={onClick} type="button" className="first:rounded-l-lg last:rounded-r-lg text-white bg-secondary-700 hover:bg-secondary-800 focus:ring-4 focus:ring-secondary-300 font-medium text-sm px-5 py-2.5 dark:bg-secondary-600 dark:hover:bg-secondary-700 focus:outline-none dark:focus:ring-secondary-800">
+    <button onClick={onClick} type="button" className="first:rounded-l-lg last:rounded-r-lg text-white bg-brown-700 hover:bg-brown-800 focus:ring-4 focus:ring-brown-300 font-medium text-sm px-5 py-2.5 dark:bg-brown-600 dark:hover:bg-brown-700 focus:outline-none dark:focus:ring-brown-800">
       {label}
     </button>);
 };
