54615cc0c1fc9207f1d040571b8b5249639747cb
Geoff Sun <geoffsun2@gmail.com>
2023-09-24 18:19:43 -0400

    updated auth styling

--- client/main/utility/InputBar/TextInput.jsx
+++ client/main/utility/InputBar/TextInput.jsx
@@ -13,7 +13,7 @@ import React from 'react';
 
 const TextInput = ({ type, value, onChange, placeholder, className}) => {
   return (
-    <input type={type || 'text'} onChange={onChange} value={value} placeholder={placeholder} className={`first:rounded-l-lg last:rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-brown-500 focus:border-brown-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-brown-500 dark:focus:border-brown-500 ${className}`}/>
+    <input type={type || 'text'} onChange={onChange} value={value} placeholder={placeholder} className={`first:rounded-l-lg last:rounded-r-lg bg-gray-50 border border-gray-300 text-gray-50 focus:ring-brown-500 focus:border-brown-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-brown-500 dark:focus:border-brown-500 ${className}`}/>
   );
 };
 
