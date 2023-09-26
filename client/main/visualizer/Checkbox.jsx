3f3bc701193730716e38a96701be83c6fd8d60ae
Geoff Sun <geoffsun2@gmail.com>
2023-09-26 18:20:16 -0400

    style checkbox

--- client/main/visualizer/Checkbox.jsx
+++ client/main/visualizer/Checkbox.jsx
@@ -10,9 +10,9 @@ const handleToggle = (booleanValue, setBooleanValue) => {
 const CheckBox = ({displayLine, setDisplayLine, displayDonut, setDisplayDonut, displayBar, setDisplayBar}) => {
 
   return (
-    <div className='h-8 w-1/2 my-2 mx-5 py-2 bg-gray-800 rounded-lg flex'>
+    <div className='h-8 w-1/2 mt-4 mx-5 py-2 bg-custom-darkgreen  rounded-lg flex'>
       <div className='flex items-center mx-2'>
-        <input id="default-checkbox" type="checkbox" value="" defaultChecked={displayLine} onClick={() => handleToggle(displayLine, setDisplayLine)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-800 dark:border-gray-600"></input>
+        <input id="default-checkbox" type="checkbox" value="" defaultChecked={displayLine} onClick={() => handleToggle(displayLine, setDisplayLine)} className="w-4 h-4 text-gray-500 bg-gray-100 border-gray-300 rounded dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-800 dark:border-gray-600"></input>
         <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium text-gray-100 dark:text-gray-300">Line Graph</label>
       </div>
       <div className='flex items-center mx-2'>
