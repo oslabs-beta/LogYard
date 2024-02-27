0e5145c5920ff7bf5747acf02a7c5b1113c1013d
Andrew Wagner <andrewwagner@andrews-air.mynetworksettings.com>
2024-02-27 17:32:48 -0500

    Fixed typo in an import statement in FilteredLogsTable

--- client/main/dashboard/logViews/FilteredLogsTable.tsx
+++ client/main/dashboard/logViews/FilteredLogsTable.tsx
@@ -3,25 +3,26 @@
  *
  * @module  FilteredLogsTable
  * @description .jsx - Creates a table from current logs and filters
- * 
+ *
  * ************************************
  */
 
 import React from 'react';
 import { useSelector } from 'react-redux';
 import logEntryCreator from './logEntryCreator';
-import GroupEntryCreator from './groupEntryCreator';
+import GroupEntryCreator from './GroupEntryCreator';
 import Table from '../../utility/Table/Table';
 import { RootState } from '../../../state/store/store';
 
-
 const FilteredLogsTable: React.FC = () => {
-  const filteredLogs = useSelector((state: RootState)=>state.logsReducer.filteredLogs);
-  
+  const filteredLogs = useSelector(
+    (state: RootState) => state.logsReducer.filteredLogs
+  );
+
   const tableEntries = [];
   const tableHeaders = [];
 
-  if (Array.isArray(filteredLogs)){
+  if (Array.isArray(filteredLogs)) {
     tableHeaders.push('Time');
     tableHeaders.push('ID');
     tableHeaders.push('Level');
@@ -32,12 +33,10 @@ const FilteredLogsTable: React.FC = () => {
 
     const startingIndex = Math.max(0, filteredLogs.length - 101);
 
-    for (let i = startingIndex; i <  filteredLogs.length ; i++){
+    for (let i = startingIndex; i < filteredLogs.length; i++) {
       tableEntries.unshift(logEntryCreator(filteredLogs[i]));
     }
-    
-  }
-  else {
+  } else {
     tableHeaders.push('Key');
     tableHeaders.push('Count');
     tableHeaders.push('Inspect');
@@ -46,10 +45,8 @@ const FilteredLogsTable: React.FC = () => {
       tableEntries.unshift(GroupEntryCreator(key, value as []));
     }
   }
-  
-  return (
-    <Table displayHeaders={ tableHeaders } displayData={ tableEntries }/>
-  );
+
+  return <Table displayHeaders={tableHeaders} displayData={tableEntries} />;
 };
 
 export default FilteredLogsTable;
