2424395f82e970d77ef67ee4d2c5cdcd05b31c5c
Andrew Wagner <andrewwagner@andrews-air.mynetworksettings.com>
2024-03-14 14:20:05 -0400

    Fixed a typo in Filter test

--- client/main/dashboard/filters/Filter.test.tsx
+++ client/main/dashboard/filters/Filter.test.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Filter from './Filter';
-import { render, screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { expect, it } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 import { renderWithProviders } from '../../../../__tests__/utilities';
-
-beforeEach;
 
 it('correctly renders the Filter component', () => {
   renderWithProviders(
